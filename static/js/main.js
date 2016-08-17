843ab488ac1f16c86020bc8dd748694584b6dec4
Michele Memoli <michele@100shapes.com>
2016-08-17 12:05:13 +0100

    Added responsive nav

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,9 @@
-import "foundation-sites";
+import "foundation-sites/js/foundation.core.js";
+import "foundation-sites/js/foundation.responsiveToggle.js";
+import "foundation-sites/js/foundation.util.mediaQuery.js";
+import "foundation-sites/js/foundation.offcanvas.js";
+import "foundation-sites/js/foundation.util.triggers.js";
+import "foundation-sites/js/foundation.util.motion.js";
 import "../styles/app.scss";
 
 $(document).foundation();
