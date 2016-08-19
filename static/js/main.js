c9f3125d143b91d54741764234e8f0a38865e8d8
Michele Memoli <michele@100shapes.com>
2016-08-19 11:29:16 +0100

    Made Video Banner responsive.

--- static/js/main.js
+++ static/js/main.js
@@ -4,6 +4,8 @@ import "foundation-sites/js/foundation.util.mediaQuery.js";
 import "foundation-sites/js/foundation.offcanvas.js";
 import "foundation-sites/js/foundation.util.triggers.js";
 import "foundation-sites/js/foundation.util.motion.js";
+import "foundation-sites/js/foundation.interchange.js";
+import "foundation-sites/js/foundation.util.timerAndImageLoader.js";
 import "../styles/app.scss";
 
 $(document).foundation();
