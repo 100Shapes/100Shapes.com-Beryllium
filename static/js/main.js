337852497a8c34e40cb30e5afdc24d69f05b1ff2
100 Shapes <100shapes@Christeans-iMac.local>
2016-12-08 12:47:54 +0000

    Fixed javascript error from foundation

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,7 @@
 import "../styles/app.scss";
 import "animate.css";
 
-import "foundation-sites/js/foundation.core.js";
-import "foundation-sites/js/foundation.responsiveToggle.js";
-import "foundation-sites/js/foundation.util.mediaQuery.js";
-import "foundation-sites/js/foundation.offcanvas.js";
-import "foundation-sites/js/foundation.util.triggers.js";
-import "foundation-sites/js/foundation.util.motion.js";
-import "foundation-sites/js/foundation.interchange.js";
-import "foundation-sites/js/foundation.util.timerAndImageLoader.js";
+import "foundation";
 
 import WOW from "wowjs";
 
